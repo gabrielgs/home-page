e388c6b90699a5a0d676e33a8812921d9fa3c0c6
Gabriel Garcia <gabrielgsdev@gmail.com>
2019-01-26 19:39:20 -0600

    Animation progress

--- src/js/index.js
+++ src/js/index.js
@@ -1,22 +1,47 @@
+import 'intersection-observer';
+
 (function () {
+
   const $toggleMenu = document.getElementById('js-toggle-mobile-menu');
   const $toggleDropdown = document.getElementById('js-open-dropdown');
   const $dropdown = document.getElementById('js-dropdown');
   const $mainNav = document.getElementById('js-main-nav');
+  const progressbars = document.querySelectorAll('.progressbar .fill');
 
-  $toggleMenu.addEventListener('click', (e) => {
-    e.preventDefault();
+  window.addEventListener('load', () => {
 
-    $mainNav.classList.toggle('open');
-  })
+    $toggleMenu.addEventListener('click', (e) => {
+      e.preventDefault();
 
-  $toggleDropdown.addEventListener('click', (e) => {
-    e.preventDefault();
+      $mainNav.classList.toggle('open');
+    })
 
-    $dropdown.classList.toggle('open');
-    e.target.classList.toggle('open');
-  })
+    $toggleDropdown.addEventListener('click', (e) => {
+      e.preventDefault();
 
-  console.log($dropdown, $mainNav);
+      $dropdown.classList.toggle('open');
+      e.target.classList.toggle('open');
+    })
 
+    createObserver();
+  }, false);
+
+
+  function createObserver() {
+    let observer = new IntersectionObserver(onIntersection);
+
+    progressbars.forEach( bar => {
+      observer.observe(bar);
+    })
+  }
+
+  function onIntersection(entries) {
+    entries.forEach(entry => {
+      if (entry.intersectionRatio > 0) {
+        entry.target.classList.add('show');
+      } else {
+        entry.target.classList.remove('show');
+      }
+    })
+  }
 })();
